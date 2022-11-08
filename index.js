9eb61ebbd5a6f4d58958908511e8968dbeac5aad
Al Noman Mahib <alnomanmatubber2001@gmail.com>
2022-11-08 18:08:39 +0600

    card added

--- index.js
+++ index.js
@@ -10,12 +10,27 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ywkglu3.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+
+async function run() {
+  try {
+    const therapyCollection = client.db("physiotherapy").collection("services");
+
+    //get all services form db
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = therapyCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+run().catch((error) => console.error(error));
 
 app.get("/", (req, res) => {
   res.send("Physiotherapy server is running");
