04cfb83e2fcb2ef8000a47a18829c81937ce50fb
Al Noman Mahib <alnomanmatubber2001@gmail.com>
2022-11-09 01:11:27 +0600

    review details added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -19,13 +19,46 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const therapyCollection = client.db("physiotherapy").collection("services");
+    //review collection
+    const reviewCollection = client.db("physiotherapy").collection("reviews");
 
-    //get all services form db
+    //3 services api
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = therapyCollection.find(query);
+      const service = await cursor.limit(3).toArray();
+      res.send(service);
+    });
+    //all services api
     app.get("/services", async (req, res) => {
       const query = {};
       const cursor = therapyCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
+    });
+
+    //single service api
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await therapyCollection.findOne(query);
+      res.send(service);
+    });
+
+    //review api
+
+    app.get("/reviews", async (req, res) => {
+      let query = {};
+      const cursor = reviewCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
+
+    app.post("/reviews", async (req, res) => {
+      console.log(req);
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
     });
   } finally {
   }
